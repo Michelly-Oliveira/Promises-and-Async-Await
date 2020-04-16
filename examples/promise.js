f828b9b39a5900f0c7a8900c7e0c21383a608076
Michelly <43606708+Michelly-Oliveira@users.noreply.github.com>
2020-04-16 10:23:51 -0300

    Added some more comments

--- examples/promise.js
+++ examples/promise.js
@@ -1,10 +1,16 @@
 // Display content on screen
 const container = document.querySelector('.container');
+// API to get data from
 const url = `https://api.github.com/users`;
 
 function fetchUsersAvatars() {
+    // Returns a promise
     return fetch(url)
+        // Stream object = A stream represents a sequence of objects (usually bytes, but not necessarily so)
+        // response is a Stream object, which means that when we call the json() method, a Promise is returned since the reading of the stream will happen asynchronously
+        // Returns a promise that will resolve into the actual data
         .then(response => response.json())
+        // Here we get the actual data that we can work with
         .then(users => {
             container.innerHTML = users.map(user => {
                 return `
@@ -14,5 +20,6 @@ function fetchUsersAvatars() {
         })
 }
 
+// Returns a promise that resolves into the avatars of the users
 const result = fetchUsersAvatars();
 console.log(result)
